abedd4f07eb8e44c6cc7c50d0bcb77dd4a6268a9
Al3xsandro <alexs123655@gmail.com>
2021-12-19 23:39:48 -0300

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './shared/infra/http/app.modules';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('MyBooks API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
